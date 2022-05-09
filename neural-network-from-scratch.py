ac89c921cf46500fe3f4410eb409966e980cf7e4
davebecoding <d01001010@csu.fullerton.edu>
2022-05-08 20:10:27 -0700

    staging neural network

--- neural-network-from-scratch.py
+++ neural-network-from-scratch.py
@@ -31,3 +31,14 @@ class Operation:
         """
 
         pass
+
+
+class add(Operation):
+    def __init__(self, x, y):
+
+        super().__init__([x, y])
+
+    def compute(self, x_var, y_var):
+
+        self.inputs = [x_var, y_var]
+        return x_var + y_var

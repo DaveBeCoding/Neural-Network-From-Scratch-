8c943fb9524f7f9ea6254e86523281bbf6c94b22
davebecoding <d01001010@csu.fullerton.edu>
2022-05-08 20:13:41 -0700

    staging neural network

--- neural-network-from-scratch.py
+++ neural-network-from-scratch.py
@@ -76,3 +76,16 @@ class Placeholder:
         self.output_nodes = []
 
         _default_graph.placeholders.append(self)
+
+
+class Variable:
+    """
+    This variable is a changeable parameter of the Graph.
+    """
+
+    def __init__(self, initial_value=None):
+
+        self.value = initial_value
+        self.output_nodes = []
+
+        _default_graph.variables.append(self)

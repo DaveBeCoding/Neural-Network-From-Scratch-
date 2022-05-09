9e6a1d238e6ae34c9a3b7b83642823826025affe
davebecoding <d01001010@csu.fullerton.edu>
2022-05-08 20:08:05 -0700

    staging neural network

---
+++ neural-network-from-scratch.py
@@ -0,0 +1,33 @@
+class Operation:
+    """
+    An Operation is a node in a "Graph". TensorFlow will also use this concept of a Graph.
+
+    This Operation class will be inherited by other classes that actually compute the specific
+    operation, such as adding or matrix multiplication.
+    """
+
+    def __init__(self, input_nodes=[]):
+        """
+        Intialize an Operation
+        """
+        self.input_nodes = input_nodes  # The list of input nodes
+        self.output_nodes = []  # List of nodes consuming this node's output
+
+        # For every node in the input, we append this operation (self) to the list of
+        # the consumers of the input nodes
+        for node in input_nodes:
+            node.output_nodes.append(self)
+
+        # There will be a global default graph (TensorFlow works this way)
+        # We will then append this particular operation
+        # Append this operation to the list of operations in the currently active default graph
+        _default_graph.operations.append(self)
+
+    def compute(self):
+        """
+        This is a placeholder function. It will be overwritten by the actual specific operation
+        that inherits from this class.
+
+        """
+
+        pass

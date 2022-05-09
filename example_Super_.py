8bf3cdd27864ea540f5c147eb45e1c5db44ad548
davebecoding <d01001010@csu.fullerton.edu>
2022-05-08 19:48:50 -0700

    testing super()

--- example_Super_.py
+++ example_Super_.py
@@ -1,8 +1,11 @@
 class SimpleClass():
     
-    def_init__(self, str_input)
-        print('SIMPLE' + str_input)
+    def __init__(self,str_input):
+        print("SIMPLE"+str_input)
 
 class ExtendedClass(SimpleClass):
-    def __init__():
+    
+    def __init__(self):
         print('EXTENDED')
+
+s = ExtendedClass()

560a7fc404c4e0f8bff802679b3f130be026298c
davebecoding <d01001010@csu.fullerton.edu>
2022-05-08 19:25:42 -0700

    setting up example of using super()

--- example_Super_.py
+++ example_Super_.py
@@ -1,4 +1,8 @@
 class SimpleClass():
     
     def_init__(self, str_input)
-        print("SIMPLE" + str_input)
+        print('SIMPLE' + str_input)
+
+class ExtendedClass(SimpleClass):
+    def __init__():
+        print('EXTENDED')
